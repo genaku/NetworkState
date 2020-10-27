15dc0bed01432386ca7d54c4a0997ff94e45fe48
Gennadiy Kuchergin <gena_kuchergin@mail.ru>
2020-10-27 15:17:00 +0300

    add doc

--- networkstate/src/main/java/com/genaku/networkstate/INetworkStateFlow.kt
+++ networkstate/src/main/java/com/genaku/networkstate/INetworkStateFlow.kt
@@ -2,7 +2,20 @@ package com.genaku.networkstate
 
 import kotlinx.coroutines.flow.StateFlow
 
+/**
+ * Interface for network online state observer
+ *
+ * @author Gena Kuchergin
+ */
 interface INetworkStateFlow: StateFlow<Boolean> {
+
+    /**
+     * Start observing
+     */
     fun start()
+
+    /**
+     * Stop observing
+     */
     fun stop()
 }
--- networkstate/src/main/java/com/genaku/networkstate/NetworkStateFlowExt.kt
+++ networkstate/src/main/java/com/genaku/networkstate/NetworkStateFlowExt.kt
@@ -14,6 +14,11 @@ val defaultNetworkRequest: NetworkRequest = NetworkRequest.Builder()
     .addTransportType(NetworkCapabilities.TRANSPORT_ETHERNET)
     .build()
 
+/**
+ * Get network online state coroutine state flow
+ *
+ * @return network online state coroutine state flow
+ */
 fun getNetworkStateFlow(context: Context): INetworkStateFlow =
     when {
         Build.VERSION.SDK_INT >= Build.VERSION_CODES.N -> NougatNetworkStateFlow(context)
